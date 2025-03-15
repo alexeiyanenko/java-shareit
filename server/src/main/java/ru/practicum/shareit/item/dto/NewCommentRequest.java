857ba4cd3e26c869a7b679a70a4d61fb79597135
Alexei Yanenko <yanenkome@yandex.ru>
2025-03-15 22:14:45 +0300

    Исправить недочеты после ревью, добавить тесты

--- server/src/main/java/ru/practicum/shareit/item/dto/NewCommentRequest.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/NewCommentRequest.java
@@ -1,10 +1,12 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class NewCommentRequest {
     private String text;
 }
